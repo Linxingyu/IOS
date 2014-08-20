89e1e052fcfd2583672620b5fe21a748e731f1bf
Vince <vince@Vinces-MacBook-Pro.local>
2014-08-20 10:42:01 +0800

    Initial Commit

---
+++ IOS/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  IOS
+//
+//  Created by Vince on 8/20/14.
+//  Copyright (c) 2014 seabig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
